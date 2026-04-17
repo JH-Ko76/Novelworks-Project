3faef1f5e2bea5c9962cd1bb0fee2e6062fd6076
JH-Ko <72744580+JH-Ko76@users.noreply.github.com>
2026-04-17 12:45:44 +0900

    Update main.tf

--- back_end/main.tf
+++ back_end/main.tf
@@ -120,18 +120,13 @@ resource "aws_api_gateway_integration_response" "options_ask_response" {
   resource_id = aws_api_gateway_resource.proxy.id
   http_method = aws_api_gateway_method.options_ask.http_method
   status_code = aws_api_gateway_method_response.options_ask_200.status_code
-
-response_parameters = {
+  response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
     "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
+  depends_on = [aws_api_gateway_integration.options_ask_integration]
 }
-
-depends_on = [
-  aws_api_gateway_integration.options_ask_integration,
-  aws_api_gateway_method_response.options_ask_200
-]
 
 # CORS設定：/updateリソース用
 resource "aws_api_gateway_method" "options_update" {
@@ -171,7 +166,7 @@ resource "aws_api_gateway_integration_response" "options_update_response" {
     "method.response.header.Access-Control-Allow-Methods" = "'PATCH,OPTIONS,GET'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
-  depends_on = [aws_api_gateway_integration.options_update_integration, aws_api_gateway_method_response.options_update_200]
+  depends_on = [aws_api_gateway_integration.options_update_integration]
 }
 
 # デプロイおよびアクセス権限
@@ -227,5 +222,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.classifier_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.classifier_api.execution_arn}/prod/*/*"
+  source_arn = "${aws_api_gateway_rest_api.classifier_api.execution_arn}/prod/*/*"
 }
